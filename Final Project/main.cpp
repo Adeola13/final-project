d16321c5ca2a4ccd23c6b583f57e69740078f723
Adeola <adeolamorren@live.nl>
2019-05-15 20:11:17 -0700

    Initial Commit

---
+++ Final Project/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Final Project
+//
+//  Created by Adeola Morren on 5/15/19.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
