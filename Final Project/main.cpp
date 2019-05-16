f592d54e0a5efe8a2a7bb926efa5d7b24c1a3cea
Adeola <adeolamorren@live.nl>
2019-05-15 20:12:16 -0700

    This is the setup project

--- Final Project/main.cpp
+++ Final Project/main.cpp
@@ -1,14 +1,8 @@
-//
-//  main.cpp
-//  Final Project
-//
-//  Created by Adeola Morren on 5/15/19.
-//
-
 #include <iostream>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
-    return 0;
+using namespace std;
+
+int main ()
+{
+    cout << "hello" << endl;
 }
